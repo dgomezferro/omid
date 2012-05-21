3cfbf7eb1d6afe7e3f7d1952dc97c78e513908b7
Maysam Yabandeh <maysam@yahoo-inc.com>
2012-05-21 17:17:02 +0200

    fix the bug of multiple outstanding transactions per client
    fix #14

---
+++ src/main/java/com/yahoo/omid/client/DeleteEfficientQueue.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2011 Yahoo! Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. See accompanying LICENSE file.
+ */
+
+package com.yahoo.omid.client;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.NoSuchElementException;
+import java.util.LinkedList;
+
+//This class enables queues with efficient random deletes
+//The deletes are delayed until they reach the head of the queue
+//The top is always valid (not deleted)
+public class DeleteEfficientQueue<E> {
+    protected Queue<E> queue;
+    protected Set<E> deletedSet;
+
+    public DeleteEfficientQueue() {
+        queue = new LinkedList<E>();
+        deletedSet = new HashSet<E>();
+    }
+
+    public E element() throws NoSuchElementException {
+        return element();
+    }
+
+    public boolean offer(E o) {
+        boolean res = queue.offer(o);
+        return res;
+    }
+
+    public E peek() {
+        return queue.peek();
+    }
+
+    /*
+     * Also remove the next tops that are already deleted
+     */
+    public E poll() {
+        E top =  queue.poll();
+        //Now remove the next tops until you find one that is not deleted
+        garbageCollectTops();
+        return top;
+    }
+
+    public E remove() throws NoSuchElementException{
+        E top =  queue.remove();
+        garbageCollectTops();
+        return top;
+    }
+
+    //remove the tops till you get one that is not deleted
+    protected void garbageCollectTops() {
+        E nexttop = queue.peek();
+        while (nexttop != null && deletedSet.contains(nexttop)) {
+            deletedSet.remove(nexttop);
+            queue.poll();
+            nexttop = queue.peek();
+        }
+    }
+
+    //If the object is not simply the top, delay its delete
+    public void delete(E o) {
+        E top = queue.peek();
+        assert(top != null);
+        if (top.equals(o))
+            garbageCollectTops();
+        else
+            deletedSet.add(o);
+    }
+
+    public void clear() {
+        queue.clear();
+        deletedSet.clear();
+    }
+}
+
---
+++ src/main/java/com/yahoo/omid/client/SharedAbortedSet.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2011 Yahoo! Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. See accompanying LICENSE file.
+ */
+
+package com.yahoo.omid.client;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.NoSuchElementException;
+import java.util.LinkedList;
+
+/*
+ * A set of aborted transactions that could be accessed by concurrent transactions
+ * If a transactin Ta is in the aborted list at the time transaction Tr starts, it must stay there till Tr commits/aborts. This is necessary to avoid the corner cases that Tr reads a value that had been written by Ta but since Ta is not in aborted list anymore, Tr takes the value as committed. Refer to HotDep for furthur details.
+ * The trick is to delay removal of aborts until the transactions that started before the remove invocation are finished.
+ */
+public class SharedAbortedSet {
+    protected Set<Long> aborted = new HashSet<Long>(1000);
+    protected DeleteEfficientQueue<Txn> queuedFullAborted;
+
+    public synchronized void clear() {
+        aborted.clear();
+        queuedFullAborted.clear();
+    }
+
+    public synchronized boolean contains(Long txn) {
+        return aborted.contains(txn);
+    }
+
+    //remove a txn from the aborted set
+    public synchronized void remove(Long txn) {
+        if (queuedFullAborted.peek() == null)//no transaction started
+            aborted.remove(txn);
+        else //delay the removal of the aborted txn
+            queuedFullAborted.offer(new FullAbortedTxn(txn));
+    }
+
+    //add a txn to the aborted set
+    public synchronized void add(Long txn) {
+        aborted.add(txn);
+    }
+
+    public synchronized void aTxnStarted(Long txn) {
+        queuedFullAborted.offer(new StartedTxn(txn));
+    }
+
+    public synchronized void aTxnFinished(Long txn) {
+        queuedFullAborted.delete(new StartedTxn(txn));
+        //after a transaction is finished, apply all full aborts that were receieved after the transaction start and do not have to be delayed due to other started txns
+        Txn top = queuedFullAborted.peek();
+        while (top instanceof FullAbortedTxn) {
+            aborted.remove(top.Ts);
+            queuedFullAborted.poll();
+            top = queuedFullAborted.peek();
+        }
+    }
+
+    abstract class Txn {
+        public Long Ts;
+        public Txn(Long Ts) {
+            this.Ts = Ts;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o instanceof StartedTxn && o != null) {
+                return ((StartedTxn)o).Ts == Ts;
+            }
+            return false;
+        }
+
+        Integer hash = null;
+        @Override
+        public int hashCode() {
+            if (hash == null)
+                hash = Ts.hashCode();
+            return hash;
+        }
+    }
+
+    class StartedTxn extends Txn {
+        public StartedTxn(Long Ts) {
+            super(Ts);
+        }
+    }
+
+    class FullAbortedTxn extends Txn {
+        public FullAbortedTxn(Long Ts) {
+            super(Ts);
+        }
+    }
+}
+
